57e70326578352894bbbc80f0f4afad2acb5a0ff
Sharon Mun <sharonymun@gmail.com>
2018-06-13 18:25:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2
    array.join(" and ")
   else 
-   array[0...-1].join(", ") << ("and ") << array[-1]
+   array[0...-1].join(", ") << (" and ") << array[-1]
  end
 end
 
