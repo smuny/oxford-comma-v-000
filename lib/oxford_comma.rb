c923bb66b4ca3e70789b9a6a6362ac07f24acc67
Sharon Mun <sharonymun@gmail.com>
2018-06-13 16:10:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,3 +2,7 @@ def oxford_comma(array)
   array.join
   array.to_a
 end
+
+# how to use these commands with 1 element, 2 element and multiple elements
+# how is it all under one method
+# how do you pick out only the first few elements then add "and" then the last string of element
