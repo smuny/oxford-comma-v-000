0ca226a8b8f61dd23ff7ba8c9e6a135d834455ad
Sharon Mun <sharonymun@gmail.com>
2018-06-13 17:44:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,14 @@
+require 'pry'
 def oxford_comma(array)
  if array.length == 1
    array[0]
   elsif array.length == 2
    array.join(" and ")
-  elsif array.length == 3
-   array[0..1] << ("and") << array[2]
+  else 
+    binding.pry
  end
 end
 
 
+#array.length == 3
+   # array[0..1] << ("and") << array[2]
